a847ee25b3d5179da4f6886dbedf6f26a4ac42cd
maximgonchar <maxgonchar9@gmail.com>
2016-04-09 19:30:56 +0300

    update main .ino program

--- Arduino/C_AND_ARDUINO.ino
+++ Arduino/C_AND_ARDUINO.ino
@@ -1,3 +1,6 @@
+#define step 10
+#define max 235
+#define min 25
 struct LAMP
 {
   int state;
@@ -19,7 +22,7 @@ void setup()
   pinMode(LAMP1.pinG, OUTPUT);
   pinMode(LAMP1.pinR, OUTPUT);
 }
- 
+
 void loop()
 {
   if(Serial.available() > 0)
@@ -38,8 +41,11 @@ void loop()
         removeBrightness();
         break;
       case 'q':
+      case 'a':
       case 'w':
+      case 's':
       case 'e':
+      case 'd':
         changeColor(incomingByte);
         break;
     }
@@ -51,28 +57,78 @@ void turnOnOffLamp(){
 }
 
 void addBrightness(){
-  if(LAMP1.brightnessR > 205 || LAMP1.brightnessG > 205 || LAMP1.brightnessB > 205)
+  if(LAMP1.brightnessR > max || LAMP1.brightnessG > max || LAMP1.brightnessB > max)
     return;
-  LAMP1.brightnessR += 50;
-  LAMP1.brightnessG += 50;
-  LAMP1.brightnessB += 50;
+  LAMP1.brightnessR += step;
+  LAMP1.brightnessG += step;
+  LAMP1.brightnessB += step;
 }
 
-void changeColor(char ch
+//q - red
+//w - green
+//e - blue
+void changeColor(char incomingByte)
+{
+  if(incomingByte == 'q' || incomingByte == 'a')
+  {
+    if(incomingByte == 'q') {
+      if(LAMP1.brightnessR < min) {
+        return;
+      }
+      LAMP1.brightnessR -= step;
+    }
+    else if (incomingByte == 'a') {
+      if(LAMP1.brightnessR > max) {
+        return;
+      }
+      LAMP1.brightnessR += step;
+    }
+  }
+  if(incomingByte == 'w' || incomingByte == 's')
+  {
+    if(incomingByte == 'w') {
+      if(LAMP1.brightnessG < min) {
+        return;
+      }
+      LAMP1.brightnessG -= step;
+    }
+    else if (incomingByte == 's') {
+      if(LAMP1.brightnessG > max) {
+        return;
+      }
+      LAMP1.brightnessG += step;
+    }
+  }
+  if(incomingByte == 'e' || incomingByte == 'd')
+  {
+    if(incomingByte == 'e') {
+      if(LAMP1.brightnessB < min) {
+        return;
+      }
+      LAMP1.brightnessB -= step;
+    }
+    else if (incomingByte == 'd') {
+      if(LAMP1.brightnessB > max) {
+        return;
+      }
+      LAMP1.brightnessB += step;
+    }
+  }
+}
 
 void removeBrightness() {
-  if(LAMP1.brightnessR < 55 || LAMP1.brightnessG < 55 || LAMP1.brightnessB < 55)
+  if(LAMP1.brightnessR < min || LAMP1.brightnessG < min || LAMP1.brightnessB < min)
     return;
-  LAMP1.brightnessR -= 50;
-  LAMP1.brightnessG -= 50;
-  LAMP1.brightnessB -= 50;
+  LAMP1.brightnessR -= step;
+  LAMP1.brightnessG -= step;
+  LAMP1.brightnessB -= step;
 }
 
 void updateLamp() {
   if(LAMP1.state == 1) {
-    analogWrite(LAMP1.pinB, LAMP1.brightness);
-    analogWrite(LAMP1.pinG, LAMP1.brightness);
-    analogWrite(LAMP1.pinR, LAMP1.brightness);
+    analogWrite(LAMP1.pinB, LAMP1.brightnessB);
+    analogWrite(LAMP1.pinG, LAMP1.brightnessG);
+    analogWrite(LAMP1.pinR, LAMP1.brightnessR);
   }
   else {
     analogWrite(LAMP1.pinB, LOW);
